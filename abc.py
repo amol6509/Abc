55a15dbd8a45df2267d0dfc8fa98792556ede006
amol6509 <mundada.amol1996@gmail.com>
2021-04-29 17:09:30 +0530

    python file

---
+++ abc.py
@@ -0,0 +1,18 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 86E5-A742
+
+ Directory of C:\Users\amol007\Desktop\Projects
+
+28-Apr-21  09:34 PM    <DIR>          .
+28-Apr-21  09:34 PM    <DIR>          ..
+28-Apr-21  09:34 PM                 0 abc.py
+28-Apr-21  06:26 PM                 7 aml.js
+28-Apr-21  03:45 PM                12 amol.html
+28-Apr-21  02:32 PM                81 amol.txt
+28-Apr-21  05:53 PM    <DIR>          Amol_petClinic
+22-Apr-21  08:59 PM    <DIR>          protractorDemo_Raghav
+27-Apr-21  05:22 PM    <DIR>          ProtraDemo
+28-Apr-21  06:27 PM               616 test2.txt
+28-Apr-21  09:31 PM               716 xyz.py
+               6 File(s)          1,432 bytes
+               5 Dir(s)  101,165,776,896 bytes free
---
+++ xyz.py
@@ -0,0 +1,18 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 86E5-A742
+
+ Directory of C:\Users\amol007\Desktop\Projects
+
+28-Apr-21  08:45 PM    <DIR>          .
+28-Apr-21  08:45 PM    <DIR>          ..
+28-Apr-21  06:26 PM                 7 aml.js
+28-Apr-21  03:45 PM                12 amol.html
+28-Apr-21  02:32 PM                81 amol.txt
+28-Apr-21  05:53 PM    <DIR>          Amol_petClinic
+22-Apr-21  08:59 PM    <DIR>          protractorDemo_Raghav
+27-Apr-21  05:22 PM    <DIR>          ProtraDemo
+28-Apr-21  06:27 PM               616 test2.txt
+28-Apr-21  08:45 PM                 0 xyz.py
+               5 File(s)            716 bytes
+               5 Dir(s)  101,193,555,968 bytes free
+gjhgjkh
